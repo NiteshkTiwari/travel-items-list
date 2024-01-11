77059c7bf14d815cff6c13306c0dec7ecb30c126
NiteshkTiwari <nt08802@gmail.com>
2024-01-11 22:04:42 +0530

    minor bug fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./Components/App";
+import App from "./Components/App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
